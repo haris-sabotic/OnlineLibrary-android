95626f7f4d8492467f2e230b4a81b4323b8e350a
haris-sabotic <haris.sinfo16@gmail.com>
2022-07-26 20:16:51 +0200

    Switch from activities to fragments

--- app/src/main/java/com/ets/onlinebiblioteka/MainActivity.kt
+++ app/src/main/java/com/ets/onlinebiblioteka/MainActivity.kt
@@ -2,10 +2,20 @@ package com.ets.onlinebiblioteka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.ets.onlinebiblioteka.fragments.LoginFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<LoginFragment>(R.id.main_fragment_container_view)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/ets/onlinebiblioteka/fragments/LoginFragment.kt
@@ -0,0 +1,21 @@
+package com.ets.onlinebiblioteka.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ets.onlinebiblioteka.R
+
+class LoginFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
